d86378ac43294f61fd5dbaff19c6f38ef7c86601
Kashif1Naqvi <naqvishah582@gmail.com>
2020-03-07 09:28:46 +0500

    login form test

--- src/Login.js
+++ src/Login.js
@@ -1,17 +1,49 @@
 import React,{Component} from 'react'
 
 class Login extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      email:'',
+      password:''
+    }
+  }
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name] : e.target.value
+    })
+  }
+  handleSubmit =aysnc (e) => {
+    e.preventDefault()
+    let formData = {
+      email:this.state.email,
+      password:this.state.password
+    }
+    let responce = await fetch('http://motor.nexasolution.com/login.php',{
+      mode:'cors',
+      reffer:'no-reffer',
+      headers:{
+        'Content-Type':'application/json',
+        'Accept':'application/json'
+      },
+      credential:'follow',
+      body:JSON.stringify(formData)
+    })
+    console.log(responce);
+
+  }
   render(){
     return(
-      <form>
+      <form onSubmit={this.handleSubmit} >
         <div>
           <label htmlFor="Email" >EMAIL ADDRESS</label>
-          <input value={} onChange={} placeholder="Email " />
+          <input type="text" name="email" value={this.state.email} onChange={this.handleChange} placeholder="Email " />
         </div>
         <div>
           <label htmlFor="Password" >PASSWORD</label>
-          <input value={} onChange={} placeholder="Email" name="shah" />
+          <input type="password" value={this.state.password} onChange={this.handleChange} placeholder="Email" name="shah" />
         </div>
+        <input type="submit" value="Login" />
       </form>
     )
   }
