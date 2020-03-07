acd50eb3a5b59b8db2d800aedb1468643863e2b2
Kashif1Naqvi <naqvishah582@gmail.com>
2020-03-07 09:19:58 +0500

    form structure setup

---
+++ src/Login.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react'
+
+class Login extends Component{
+  render(){
+    return(
+      <form>
+        <div>
+          <label htmlFor="Email" >EMAIL ADDRESS</label>
+          <input value={} onChange={} placeholder="Email " />
+        </div>
+        <div>
+          <label htmlFor="Password" >PASSWORD</label>
+          <input value={} onChange={} placeholder="Email" name="shah" />
+        </div>
+      </form>
+    )
+  }
+}
+export default Login
